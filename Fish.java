e18b3b2f2afce719f35ffff6dcf2a6a0150f641c
Keith-McClelland <keithmcclelland5@gmail.com>
2025-04-25 09:04:28 -0400

    Added fish class

---
+++ Fish.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fish here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fish extends Actor
+{
+    /**
+     * Act - do whatever the Fish wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(-10);
+        
+        if(getX() <= 0) {
+            resetFish();
+        }
+    }
+    public void resetFish() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600,100);
+        } else {
+            setLocation(600,300);
+        }
+    }
+}
